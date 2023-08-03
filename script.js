8c92913d2087c1ecbdeee1b52fcedcb6fe721f3e
Michael <mikelz129@gmail.com>
2023-08-03 19:40:05 -0400

    made some stuff

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 // Global Variables
-let number;
 let operator;
 let secNum;
-let displayValue = 80085;
+let displayValue = 0;
 
 // Main Functions
 const add = function(x,y) {
@@ -21,19 +20,72 @@ const divide = function(x,y) {
     return x / y
 }
 
-const operate = function(x,func,y) {
-    return func(x,y)
+const operate = function() {
+    console.log(displayValue)
+    console.log(secNum)
+    console.log(operator)
+    switch (operator) {
+        case "sub":
+            newNum = displayValue - secNum;
+            displayValue = newNum;
+            break;
+        case "multi":
+            newNum = displayValue * secNum;
+            displayValue = newNum;
+            break;
+        case "div":
+            newNum = displayValue / secNum;
+            displayValue = newNum;
+            break;
+        case "add":
+            newNum = displayValue + secNum;
+            displayValue = newNum;
+            break;
+        }
+        console.log(displayValue)
+        const display = document.querySelector('#display');
+        display.textContent=displayValue;
 }
 
 // CALC Working
-
-const changeBlue = function() {
-    const display = document.querySelector('#display');
-    display.setAttribute('style', 'color: blue; background: white;');
-}
-
 const run = function() {
     const display = document.querySelector('#display');
     display.textContent=displayValue;
 }
 
+// NUMBERS
+const ce = function() {
+    console.log(displayValue);
+    displayValue = "";
+    console.log(displayValue);
+    const display = document.querySelector('#display');
+    display.textContent=displayValue;
+}
+
+const number = function(buttonNum) {
+    displayValue = displayValue + buttonNum;
+    const display = document.querySelector('#display');
+    display.textContent=displayValue; 
+    console.log(displayValue)    
+}
+
+// Function Buttons
+const subButton = function() {
+    operator = "sub"
+    console.log(operator)    
+}
+
+const addButton = function() {
+    operator = "add"
+    console.log(operator)    
+}
+
+const multiButton = function() {
+    operator = "multi"
+    console.log(operator)    
+}
+
+const divButton = function() {
+    operator = "div"
+    console.log(operator)    
+}
